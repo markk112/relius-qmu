afc10f239c5c58f855f1f455576f2afb57c352b2
cu-rve <72937506+cu-rve@users.noreply.github.com>
2021-03-23 20:26:18 +0000

    added fonts, css globals, nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="nav">
+        <ul>
+            <li class="heading">Home</li>
+            <li>
+                <router-link to="/">Welcome</router-link>
+            </li>
+            <li>
+                <router-link to="/about">Device</router-link>
+            </li>
+            <li class="heading">Sideload</li>
+            <li>
+                <router-link to="/here">Install</router-link>
+            </li>
+            <li class="heading">Settings</li>
+            <li>
+                <router-link to="/bring">Setup</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+}
+</script>
+
+<style>
+#nav {
+  background-color: #000000;
+  color: #fff;
+  width: 340px;
+  font-size: 14px;
+}
+
+#nav ul li.heading {
+  font-weight: 600;
+  padding-left: 20px;
+}
+
+#nav ul li a {
+  display: block;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-left: 35px;
+  font-size: inherit;
+}
+
+#nav ul li a.router-link-active {
+  background-color: #007cfd;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS reset
+// https://piccalil.li/blog/a-modern-css-reset
+import '@/assets/css/reset.css'
+
+// Global styles
+import '@/assets/css/global.css'
+
 createApp(App).use(store).use(router).mount('#app')
