8e28157a4e4e3cc5ce5da862efb7e68ee07434da
cu-rve <72937506+cu-rve@users.noreply.github.com>
2021-05-01 21:38:20 +0100

    add resets to global css

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// Reset CSS to remove default styling
-import '@/assets/css/reset.css'
-
-// Global styling
 import '@/assets/css/global.css'
 
 createApp(App).use(store).use(router).mount('#app')
